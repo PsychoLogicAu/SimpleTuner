72bc273a8b82f07edb0df1795146a134d91eca8f
PsychoLogicAu <psycho.logic.au@gmail.com>
2025-08-10 09:37:03 +0000

    mount huggingface cache dir

--- entry.sh
+++ entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source SimpleTuner/.venv/bin/activate
+echo "$ env ENV=default CONFIG_BACKEND=env bash train.sh"
 if [ "$1" = "bash" ] || [ "$1" = "/bin/bash" ]; then
     exec /bin/bash
 else
