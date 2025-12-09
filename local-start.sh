616fba830cb54c8217f4cf7d3cc8784512515cb8
PsychoLogicAu <psycho.logic.au@gmail.com>
2025-12-09 21:33:29 +0000

    update dockerfile/compose

--- local-start.sh
+++ local-start.sh
@@ -5,6 +5,6 @@ source /opt/venv/bin/activate
 cd /workspace/SimpleTuner
 
 echo "Link training config file to config/config.json, then"
-echo "$ env ENV=default CONFIG_BACKEND=env bash train.sh"
+echo "$ env ENV=default CONFIG_BACKEND=env SIMPLETUNER_LOG_LEVEL=DEBUG bash simpletuner/train.sh"
 
 exec /usr/bin/bash
